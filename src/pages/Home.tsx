515c2c8c3d7c0d9a56d223c812b7ddebe3168a02
Jovane Mafort <jmafort@id.uff.br>
2021-06-27 20:00:50 -0300

    Room features done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { useHistory } from 'react-router-dom';
+import { FormEvent } from 'react';
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
@@ -7,11 +8,14 @@ import googleIconImg from '../assets/images/google-icon.svg'
 import '../styles/auth.scss';
 import { Button } from '../components/Button';
 import { useAuth } from '../hooks/useAuth';
+import { useState } from 'react';
+import { database } from '../services/firebase';
 
 
 export function Home() {
   const history = useHistory();
   const {user, signInWithGoogle} = useAuth();
+  const [roomCode, setRoomCode] = useState('');
 
   async function handleCreateRoom(){
     if(!user){
@@ -19,6 +23,28 @@ export function Home() {
     }
 
     history.push('/rooms/new');
+  }
+
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if(roomCode.trim() === ''){
+      return;
+    }
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+    if(!roomRef.exists()){
+      alert('Room does not exists.');
+      return;
+    }
+
+    if(roomRef.val().endedAt){
+      alert('Room already closed.');
+      return;
+    }
+
+    history.push(`/rooms/${roomCode}`);
   }
 
   return (
@@ -40,10 +66,12 @@ export function Home() {
             ou entre em uma sala
           </div>
 
-          <form>
+          <form onSubmit={handleJoinRoom}>
             <input 
               type="text"
               placeholder="Digite o código da sala"
+              onChange={event => setRoomCode(event.target.value)}
+              value={roomCode}
             />
             <Button type="submit">
               Entrar na sala
